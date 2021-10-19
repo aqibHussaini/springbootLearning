d4d323b8d4a518d555fc1472ba8909f790cd7d8b
aqibHussaini <67430403+aqibHussaini@users.noreply.github.com>
2021-10-19 12:22:02 +0500

    first commit

---
+++ src/main/java/com/learning/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.learning.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home/{name}")
+    public String Home(Model model,@PathVariable String name) {
+        model.addAttribute("name",name);
+        System.out.println(name);
+        return "home.html";
+    }
+}
