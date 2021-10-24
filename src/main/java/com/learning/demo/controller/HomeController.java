442827790cb9de7b269a6fc8c45dbf86f76770b0
aqibHussaini <67430403+aqibHussaini@users.noreply.github.com>
2021-10-24 18:49:20 +0500

    Update HomeController.java
    name modified

--- src/main/java/com/learning/demo/controller/HomeController.java
+++ src/main/java/com/learning/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HomeController {
     @GetMapping("/home/{name}")
-    public String Home(Model model,@PathVariable String name) {
+    public String Home(Model model,@PathVariable String Name) {
         return "home.html";
     }
 }
